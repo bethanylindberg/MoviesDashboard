d8003f2a3ef205e7f22320b3faa12cb4db55493d
Bethany Lindberg <bethfisch@gmail.com>
2019-06-21 20:07:31 -0500

    clean up files

--- static/js/dotplot.js
+++
@@ -1,9 +0,0 @@
-// Let selection equal selected Actor or Actress
-let selection = "Morgan Freeman";
-let url = "query.json";
-let dropdown = "dropdown.json";
-drawBoxOffice('#boxoffice',url);
-drawDotplot('#dotplot',url);
-drawWordCloud('#wordcloud',url);
-
-
